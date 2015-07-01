6b9b592a639349ad1f6262a3325003b4caa89047
abhishekkt <abha.abhi@gmail.com>
2015-07-01 15:49:08 +0530

    Views mapped to the urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from blog.models import blogger,blog
+from django.shortcuts import render_to_response, get_object_or_404
+
+
+def index(request):
+	return render_to_response('index.html',{
+		'blog': blog.objects.all()
+		})
+
+def view_post(request,slug):
+	return render_to_response('view_post.html',{
+		'blog': get_object_or_404(blog, slug=slug)
+		})
 
 # Create your views here.
--- myblogger/urls.py
+++ myblogger/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^$','blog.views.index'),
+	url(r'blog/view/(?P<slug>[^\.]+).html','blog.views.view_post',name='view_blog_post'),	
 ]
